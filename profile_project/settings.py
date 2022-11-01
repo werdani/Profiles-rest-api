46a92bab728426a115e8280e576f57ae2aa3d44b
werdani <ammaryasser554zz@gmail.com>
2022-11-01 10:39:02 +0200

    [ADD] license file

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # new apps 
+    'rest_framework',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
