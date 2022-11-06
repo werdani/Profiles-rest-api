9d3bb0070b9e22d11ecf7b9b2ffcb0da9fd3588d
werdani <ammaryasser554zz@gmail.com>
2022-11-06 16:53:32 +0200

    [UPDATE] django admin

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # new apps 
+    # news apps 
     'rest_framework',
     'profiles_api',
 ]
