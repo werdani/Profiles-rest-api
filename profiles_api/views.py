eba367dfae22da630e1bee5d305b4461c68b551c
werdani <ammaryasser554zz@gmail.com>
2022-11-13 16:22:12 +0200

    [CREATE] search filtering

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from profiles_api import serializers
 from rest_framework import viewsets
 from profiles_api import models, permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -93,3 +94,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
