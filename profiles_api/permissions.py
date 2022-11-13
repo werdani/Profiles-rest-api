6af54f084c6f5c3436235c2776c2b53ffa4a6bf6
werdani <ammaryasser554zz@gmail.com>
2022-11-13 16:05:54 +0200

    [CREATE] permissions

---
+++ profiles_api/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """Allow user to edit own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """check user is trying to edit their own profile"""
+        if request.mehtod in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
+        #return super().has_object_permission(request, view, obj)
+
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
-from profiles_api import models
+from profiles_api import models, permissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class HelloApiView(APIView):
@@ -90,3 +91,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
